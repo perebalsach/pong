1fa5f5ca1f5cbfeca768336575a3b077cb7e6f51
Pere Balsach <pere.balsach@gmail.com>
2019-02-25 21:51:42 +0100

    added

---
+++ Assets/Scripts/BubbleManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BubbleManager : MonoBehaviour {
+
+    public GameObject []bubbles;
+    public bool GameOver = true;
+
+    [SerializeField]
+    float SpawnInterval;
+
+    [SerializeField]
+    float SpawnRangeDistance;
+
+	void Start ()
+    {
+        StartSpawnBubbles();
+	}
+	
+	void Update ()
+    {
+
+	}
+
+    void SpawnBubble()
+    {
+        int bubbleId = Random.Range(0, bubbles.Length);
+
+        float randNum = Random.Range(-SpawnRangeDistance, SpawnRangeDistance);
+        Vector3 randPos = new Vector3(randNum, transform.position.y, transform.position.z);
+
+        GameObject bubble = Instantiate(bubbles[bubbleId], randPos, transform.rotation);
+    }
+
+    IEnumerator SpawnBubbles()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (true)
+        {
+            SpawnBubble();
+            yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+
+    public void StartSpawnBubbles()
+    {
+        StartCoroutine("SpawnBubbles");
+    }
+
+    public void StopSpawnBubbles()
+    {
+        StopCoroutine("SpawnBubbles");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject bubble;
+    public GameObject bullet;
+    bool canMove = true;
+
+    [SerializeField]
+    float moveSpeed;
+    private bool splitBubble = false;
+
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(canMove)
+        {
+            Move();
+        }
+        if(splitBubble)
+        {
+            splitBubbles();
+        }
+	}
+    void splitBubbles()
+    {
+        GameObject splitBubble1 = Instantiate(bubble, transform.position, transform.rotation);
+        //splitBubble1.AddComponent<Rigidbody2D>();
+        Rigidbody2D rb = splitBubble1.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, 10f));
+        splitBubble = false;
+    }
+
+    void Move()
+    {
+        float xPos = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * xPos * moveSpeed * Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+        splitBubble = true;
+    }
+}
